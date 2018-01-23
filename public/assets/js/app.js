00a06bf6c04356a1b7a6ef242f92062b40922c58
estrane <zeldrew@hotmail.com>
2018-01-23 14:16:58 -0800

    partial item search working

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,27 +1,58 @@
 $(function () {
-    
-     $("#rateYo").rateYo({
-       rating: 2.5,
-       halfStar: true
-     });
-    
-     $("#add-btn").on("click", function (event) {
-         event.preventDefault();
-         var rating = $("#rateYo").rateYo("rating");
-         console.log("Rating is: " + rating);
 
-         var newRating = {
-             item: $("#item").val().trim(),
-             category: $("#category").val().trim(),
-             comment: $("#comment").val().trim(),
-             rating: rating,
-             UserId: 1
-         };
+    $("#rateYo").rateYo({
+        rating: 2.5,
+        halfStar: true
+    });
 
-         $.post("api/rating/create", newRating, function() {
-             console.log("Added a rating!");
-             window.location.href="/";
-         });
-     });
+    $("#add-btn").on("click", function (event) {
+        event.preventDefault();
+        var rating = $("#rateYo").rateYo("rating");
+        console.log("Rating is: " + rating);
 
-   });
+        var newRating = {
+            item: $("#item").val().trim(),
+            category: $("#category").val().trim(),
+            comment: $("#comment").val().trim(),
+            rating: rating,
+            UserId: 2
+        };
+
+        $.post("api/rating/create", newRating, function () {
+            console.log("Added a rating!");
+            window.location.href = "/";
+        });
+    });
+
+    $("#search-item").on("click", function (event) {
+        event.preventDefault();
+
+        $("#searched").empty();
+
+        var searchItem = $("#item-search").val().trim();
+
+        $.get("/item/" + searchItem, function (data) {
+            console.log("Looking it up.")
+
+            console.log(data);
+
+            if (data.length !== 0) {
+
+                
+
+                    var row = $("<div>");
+                    row.addClass("item");
+
+                    row.append("<div class='panel panel-default'><div class='panel-heading'>" +
+                    "<h2 class='panel-title'>" + data.ratings.User.username + "</h2>" +
+                    "<p class='rating'>" + data.ratings.rating + "<span class='glyphicon glyphicon-star' aria-hidden='true'></span></p></div>" +
+                    "<div class='panel-body'><p>" + data.ratings.comment + "</p></div></div>");
+
+                    $("#searched").prepend(row);
+
+            }
+        });
+        $("#item-search").val("");
+    });
+
+});
--- routes/HTML-routes.js
+++ routes/HTML-routes.js
@@ -6,4 +6,10 @@ module.exports = function(app) {
       app.get("/add", function(req, res) {
         res.render("add");
       });
+
+      app.get("/item", function(req, res) {
+          res.render("item");
+      });
+
+
 }
