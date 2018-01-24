0cf40cd1b5fa19c80a574220eb3fd92718a99ef4
Dominic San Filippo <jaxin@Dominics-MacBook-Pro.local>
2018-01-24 11:41:59 -0800

    Added all users route

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,40 +1,32 @@
 //
 var db = require("../models");
 //
-module.exports = function(app) {
+module.exports = function (app) {
     // GET route for all users
-    app.get("/user", function(req, res) {
+    app.get("/user", function (req, res) {
         db.User.findAll({
             include: db.Rating
-        }).then(function(ratings) {
+        }).then(function (ratings) {
             res.json(ratings);
         });
     });
     // POST route for adding a user
-<<<<<<< HEAD
-    app.post("/api/users/create", function(req, res) {
-=======
-    app.post("/api/user/create", function(req, res) {
->>>>>>> master
-        db.User.create(req.body).then(function(result) {
-          console.log("Added: " + req.body);
-          res.json(result);
+    app.post("/api/user/create", function (req, res) {
+        db.User.create(req.body).then(function (result) {
+            console.log("Added: " + req.body);
+            res.json(result);
         });
-      });
-      // GET method for finding a specific author
-<<<<<<< HEAD
-      app.get("/api/users/:id", function(req, res) {
-=======
-      app.get("/api/user/:id", function(req, res) {
->>>>>>> master
+    });
+    // GET method for finding a specific author
+    app.get("/api/user/:id", function (req, res) {
         db.User.findOne({
-          where: {
-            id: req.params.id
-          },
-          include: db.Ratings
-        }).then(function(ratings) {
-          res.json(ratings);
+            where: {
+                id: req.params.id
+            },
+            include: db.Ratings
+        }).then(function (ratings) {
+            res.json(ratings);
         });
-      });
+    });
 }
 //
