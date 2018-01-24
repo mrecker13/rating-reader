d1cf94d2c232932cd13b0634f771748b21f7dfc2
Dominic San Filippo <jaxin@Dominics-MacBook-Pro.local>
2018-01-24 11:31:10 -0800

    .gitignore updated and rating routes

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -2,31 +2,31 @@
 var db = require("../models");
 //
 module.exports = function(app) {
-    // GET route for all items
-    app.get("/api/users/all", function(req, res) {
+    // GET route for all users
+    app.get("/user", function(req, res) {
         db.User.findAll({
             include: db.Rating
         }).then(function(ratings) {
             res.json(ratings);
         });
     });
-    // POST route for adding an item
-    app.post("/api/users/create", function(req, res) {
+    // POST route for adding a user
+    app.post("/api/user/create", function(req, res) {
         db.User.create(req.body).then(function(result) {
           console.log("Added: " + req.body);
           res.json(result);
         });
       });
-
-      // GET method for fining a specific post
-      app.get("/api/users/:id", function(req, res) {
-        db. User.findOne({
+      // GET method for finding a specific author
+      app.get("/api/user/:id", function(req, res) {
+        db.User.findOne({
           where: {
             id: req.params.id
           },
-          include: [db.Ratings]
+          include: db.Ratings
         }).then(function(ratings) {
           res.json(ratings);
         });
       });
 }
+//
